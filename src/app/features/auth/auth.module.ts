11c65520ca2a7c6939d3c0054a59dc8af93baf96
TuDeThuong <nguyenanhtu1604@gmail.com>
2022-02-22 01:50:20 +0100

    update login-signup-logout

---
+++ src/app/features/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+@NgModule({
+  declarations: [LoginFormComponent, LoginPageComponent, RegisterPageComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+})
+export class AuthModule {}
