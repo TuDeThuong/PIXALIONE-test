3648ab15dbadbeb2a7e36cff0c1079757098c0cd
TuDeThuong <nguyenanhtu1604@gmail.com>
2022-02-22 01:18:51 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'pixalion',
+    appId: '1:237390137365:web:45f53273e457ebccefe61c',
+    storageBucket: 'pixalion.appspot.com',
+    apiKey: 'AIzaSyAqo0t3FYzKQizdZMPIES1VK3x1juds854',
+    authDomain: 'pixalion.firebaseapp.com',
+    messagingSenderId: '237390137365',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pixalion',
+    appId: '1:237390137365:web:45f53273e457ebccefe61c',
+    storageBucket: 'pixalion.appspot.com',
+    apiKey: 'AIzaSyAqo0t3FYzKQizdZMPIES1VK3x1juds854',
+    authDomain: 'pixalion.firebaseapp.com',
+    messagingSenderId: '237390137365',
+  },
   production: false
 };
 
